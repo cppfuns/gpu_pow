20c780a2371965d026d0b714aca419c1d53d25b8
Surou <cppfuns@gmail.com>
2023-09-04 21:40:58 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,37 @@
-extern crate rust_cuda;
-extern crate rayon;
-
-use rust_cuda::prelude::*;
-use rayon::prelude::*;
-use std::time::{Instant, Duration};
-
-const N: usize = 1_000_000; // 向量的大小
-const NUM_GPUS: usize = 2; // 使用的显卡数量
+extern crate cuda_sys as cuda;
+use std::ffi::CString;
 
 fn main() {
-    // 初始化 CUDA 上下文
-    let contexts: Vec<cuda::Context> = (0..NUM_GPUS)
-        .map(|i| cuda::Context::new(i as i32))
-        .collect();
+    unsafe {
+        // 初始化 CUDA
+        cuda::cudaInit(0);
 
-    // 创建 CUDA 流
-    let streams: Vec<cuda::Stream> = contexts.iter().map(|context| cuda::Stream::new(context)).collect();
+        // 获取可用的 GPU 设备数量
+        let mut device_count: i32 = 0;
+        cuda::cudaGetDeviceCount(&mut device_count);
+        println!("Found {} CUDA devices.", device_count);
 
-    // 创建输入向量和输出向量
-    let mut a = vec![1.0; N];
-    let mut b = vec![2.0; N];
-    let mut c = vec![0.0; N];
+        // 遍历每个设备
+        for device_id in 0..device_count {
+            // 选择当前设备
+            cuda::cudaSetDevice(device_id);
 
-    // 使用 Rayon 并行计算框架，将任务分发到多个 GPU 设备
-    let start_time = Instant::now();
+            // 获取设备属性
+            let mut device_prop: cuda::cudaDeviceProp = std::mem::zeroed();
+            cuda::cudaGetDeviceProperties(&mut device_prop, device_id);
 
-    (0..NUM_GPUS).into_par_iter().for_each(|i| {
-        // 获取当前 GPU 的上下文和流
-        let context = &contexts[i];
-        let stream = &streams[i];
+            let name_cstr = CString::from_raw(&device_prop.name as *const i8 as *mut i8);
+            let name = name_cstr.to_str().unwrap();
 
-        // 计算每个 GPU 的任务范围
-        let chunk_size = N / NUM_GPUS;
-        let start = i * chunk_size;
-        let end = start + chunk_size;
-
-        // 分配 GPU 内存并传输数据到 GPU
-        let a_device = cuda::Memory::from_slice(&a[start..end], stream);
-        let b_device = cuda::Memory::from_slice(&b[start..end], stream);
-        let c_device = cuda::Memory::from_mut_slice(&mut c[start..end], stream);
-
-        // 执行向量加法的 CUDA 核心
-        cuda::launch!{
-            vector_add<<<1, 256, 0, stream.clone()>>>(
-                a_device.as_device_ptr(),
-                b_device.as_device_ptr(),
-                c_device.as_device_mut_ptr(),
-                chunk_size as i32,
-            )
-        };
-
-        // 同步 CUDA 流
-        cuda::stream_sync(stream);
-    });
-
-    // 停止计时器并输出执行时间
-    let end_time = Instant::now();
-    let elapsed_time = end_time - start_time;
-    println!("GPU execution time: {:?}", elapsed_time);
-
-    // 验证 GPU 计算的结果
-    for i in 0..N {
-        assert_eq!(c[i], a[i] + b[i]);
-    }
-
-    println!("GPU computation verified.");
-}
-
-// CUDA 核心，执行向量加法
-#[no_mangle]
-extern "C" fn vector_add(a: *const f32, b: *const f32, c: *mut f32, n: i32) {
-    let index = cuda::block_idx_x() * cuda::block_dim_x() + cuda::thread_idx_x();
-    if index < n {
-        unsafe {
-            *c.offset(index as isize) = *a.offset(index as isize) + *b.offset(index as isize);
+            println!("Device {}: {}", device_id, name);
+            println!("  Compute Capability: {}.{}", device_prop.major, device_prop.minor);
+            println!("  Total Global Memory: {} bytes", device_prop.totalGlobalMem);
+            println!("  CUDA Cores: {}", device_prop.multiProcessorCount);
+            println!("  Clock Rate: {} KHz", device_prop.clockRate);
+            println!();
         }
+
+        // 清理 CUDA
+        cuda::cudaDeviceReset();
     }
 }
